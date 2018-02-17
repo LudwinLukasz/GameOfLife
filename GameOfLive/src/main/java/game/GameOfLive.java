47a6426b44fc6c44404b0c052b6b4cf0a992c47c
Lukasz Ludwin <gorli@interia.pl>
2018-02-17 11:20:21 +0100

    starting GameOfLive project, adding the first test

---
+++ GameOfLive/src/main/java/game/GameOfLive.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Created by arabk on 17.02.2018.
+ */
+public class GameOfLive {
+
+    public static int [][] nextEvolutionStep(int [][] currentCells) {
+        return null;
+    }
+
+}
---
+++ GameOfLive/src/test/java/game/GameOfLiveTest.java
@@ -0,0 +1,32 @@
+package game;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by arabk on 17.02.2018.
+ */
+public class GameOfLiveTest {
+
+    @Test
+    public void nextEvolutionStepTest() {
+        int [][] inputArray =  new int[][]{
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        int [][] result = GameOfLive.nextEvolutionStep(inputArray);
+        int [][] expectedResult = new int[][]{
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+        assertArrayEquals(expectedResult,result);
+    }
+}
